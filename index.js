91bd604b5eb8e998e3254648cfcf211b4abe8716
Ranvijay Singh Kuwarbi <ranvijay.kuwarbi@appinventiv.com>
2023-01-30 21:01:04 +0530

    added basic code for rock paper scissor

--- index.js
+++ index.js
@@ -43,8 +43,10 @@ function message(user, computer, status){
         case "Tie":
             return `It's a tie! ${user} ties with ${computer}`
         case "Lose":
+            comp++;
             return `You Lose! ${computer} beats ${user}`
         default:
+            player++;
             return `You win! ${user} beats ${computer}`
     }
 }
@@ -55,5 +57,50 @@ function message(user, computer, status){
         const computerChoice = getComputerChoice()
         console.log(playSelection(userChoice, computerChoice))
     }
-})() 
+})
+
+const buttons = Array.from(document.querySelectorAll(".btn"))
+const result = document.getElementById("result")
+const userScore = document.getElementById("user")
+const compScore = document.getElementById("comp")
+let count = 0, player = 0, comp = 0
+buttons.forEach(function (btn){
+    btn.addEventListener("click", function(e){
+        if(count == 0){
+            result.innerHTML = ""
+        }
+        count++;
+        const comChoice = getComputerChoice()
+        const text = document.createElement("h1")
+        text.classList.add("scoreBoard")
+        text.textContent = playSelection(this.id, comChoice)
+        result.appendChild(text)
+        userScore.textContent = player;
+        compScore.textContent = comp;
+        if(count == 5){
+            console.log("Game over")
+            result.innerHTML = ""
+            const text = document.createElement("h1")
+            text.classList.add("scoreBoard")
+            userScore.textContent = player;
+            compScore.textContent = comp;
+            if(player > comp){
+                text.textContent = "YOU WIN!"
+                console.log("YOU WIN!");
+            }else{
+                text.textContent = "YOU LOSE!"
+                console.log("You lose");
+            }
+            result.appendChild(text)
+            count = 0;
+            player = 0
+            comp = 0
+        }else{
+            console.log("More rounds are left...")
+        }
+        
+    })
+})
+
+
 
